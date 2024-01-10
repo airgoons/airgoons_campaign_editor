9e93fa5540e203e1af45f68a905be9ad74093e76
wonkotron <wonkotron@gmail.com>
2024-01-10 12:29:40 -0800

    Implement polygon and linestring handling

--- tools/kmz_to_json.py
+++ tools/kmz_to_json.py
@@ -40,19 +40,29 @@ def kmz_to_json(kmz_online_url=None, json_output_path="output.json", kmz_filenam
             if name in data["layer_data"][layer]:
                 print("[ERROR] Duplicate key exists in layer data:  {0}|{1}".format(layer, name))
             else:
+                entry = {
+                    "layer":            layer,
+                    "name":             name,
+                    "description":      description,
+                }
+
                 if type(geometry) == shapely.geometry.point.Point:
-                    entry = {
-                        "layer":            layer,
-                        "name":             name,
-                        "description":      description,
-                        "geometry_type":    "Point",
-                        "latitude":         geometry.y,
-                        "longitude":        geometry.x
-                    }
-                    data["layer_data"][layer][name] = entry
+                    entry["geometry_type"] = "Point"
+                    entry["latitude"] = geometry.y
+                    entry["longitude"] = geometry.x
+
+                elif type(geometry) == shapely.geometry.polygon.Polygon:
+                    entry["geometry_type"] = "Polygon"
+                    entry["coordinates"] = list(geometry.exterior.coords)
+
+                elif type(geometry) == shapely.geometry.linestring.LineString:
+                    entry["geometry_type"] = "LineString"
+                    entry["coordinates"] = list(geometry.coords)
 
                 else:
-                    print("[WARN] Geometry type not supported: ({0}|{1}):  {2}".format(layer, name, type(geometry)))  # @TODO implement proper logging
+                    print("[WARN] Geometry type not supported, data may be corrupted: ({0}|{1}):  {2}".format(layer, name, type(geometry)))  # @TODO implement proper logging
+                
+                data["layer_data"][layer][name] = entry
 
     with open(json_output_path, 'w') as json_file:
         json_file.write(json.dumps(data, indent=4))
