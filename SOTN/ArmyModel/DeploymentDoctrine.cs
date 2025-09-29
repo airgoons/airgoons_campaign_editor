383cc4e2c78333f8dd274da2a23ab57207c6277d
wonkotron <wonkotron@gmail.com>
2025-09-28 22:46:13 -0700

    initial deployment doctrine model

---
+++ SOTN/ArmyModel/DeploymentDoctrine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOTN.ArmyModel {
+    public static class DeploymentDoctrine {
+        public static class Distances {
+            // meters
+            public static readonly int DivisionToBrigade    = 3000;
+            public static readonly int BrigadeToBattalion   = 1000;
+            public static readonly int BattalionToCompany   =  500;
+            public static readonly int CompanyToPlatoon     =  100;
+        }
+    }
+}
